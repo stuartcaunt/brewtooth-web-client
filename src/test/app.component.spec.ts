f78ccce0f8177984d3c354e8e228698e287b845b
Stuart Caunt <stuart.caunt@gmail.com>
2017-08-07 22:48:20 +0200

    Fix component name for tests.

--- src/test/app.component.spec.ts
+++ src/test/app.component.spec.ts
@@ -1,30 +1,30 @@
 import { TestBed, async } from '@angular/core/testing';
 
-import { BrewtoothComponent } from '../app/brewtooth.component';
+import { AppComponent } from 'components';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        BrewtoothComponent
+        AppComponent
       ],
     }).compileComponents();
   }));
 
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(BrewtoothComponent);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(BrewtoothComponent);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(BrewtoothComponent);
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
